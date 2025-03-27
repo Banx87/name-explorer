c371ba04ea7b1cac9fabdec3c53ac82fcec01054
Banx87 <joni.lindholm@gmail.com>
2025-03-27 17:30:58 +0200

    add initial database connection and utility functions

---
+++ inc/db-connect.inc.php
@@ -0,0 +1,17 @@
+<?php
+$config = require __DIR__ . '/../config.php';
+
+try {
+    $pdo = new PDO('mysql:host=' . $config['db_host'] . ';dbname=' . $config['db_name'] . ';charset=utf8mb4', $config['db_user'], $config['db_pass'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    echo 'A problem occurred with the database connection...';
+    die();
+}
+
+// TEST
+// $stmt = $pdo->prepare('SELECT * FROM `names`');
+// $stmt->execute();
+
+// var_dump($stmt->fetch(PDO::FETCH_ASSOC));
