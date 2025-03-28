84e4f48889c7f32d6db83a78a5bdeaa1da280dfa
Banx87 <joni.lindholm@gmail.com>
2025-03-28 16:19:02 +0200

    [ADD] Refactor to include render function.

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -5,6 +5,18 @@ function e($value)
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
+function render($view, $params)
+{
+    extract($params);
+
+    ob_start();
+    require __DIR__ . '/../views/pages/' . $view . '.php';
+    $contents = ob_get_clean();
+
+    $alphabet = gen_alphabet();
+    require __DIR__ . '/../views/layouts/main.view.php';
+}
+
 // This function will generate all the letters of the alphabet as an array
 // ['A','B',C', ... 'X','Y',Z']
 function gen_alphabet(): array
